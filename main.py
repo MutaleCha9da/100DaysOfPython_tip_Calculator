99cd9a4eb1098ce20c24bb8c392e3b254075d190
Mutale Chanda <mutalefbc@gmail.com>
2022-09-21 21:37:31 +0000

    Added formula, and fixed all bugs

--- main.py
+++ main.py
@@ -6,3 +6,10 @@
 #Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
 #Write your code below this line 👇
+print("Welcome to the tip Calculator.")
+bill = float(input("What was the total bill? "))
+p_tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+bill_split = int(input("How many people to split the bill? "))
+full_tip = round((bill / bill_split) * (p_tip/100),2)
+print(f"Each person should pay: ${full_tip}") 
+
